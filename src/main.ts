39eed33f296746f77f850b2d3647b96ac61739a2
jhon <jhon.bocanegra26@gmail.com>
2023-06-27 18:43:57 -0500

    web sockets

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h2>Websocket - Client</h2>
+    <input id="jwt-token" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br/>
+    <span id="server-status">offline</span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
+  </div>
+`;
